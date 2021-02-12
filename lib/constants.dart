7c44123bc168e6d5120fa2dd68d9d15b38661b20
akirabung <55126469+akirabung@users.noreply.github.com>
2021-02-12 15:25:57 +0700

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,61 @@
+const FOOD_DATA = [
+  {
+    "name":"Chocolate Chip \nCookie Dough",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Chocolate-Chip-Cookie-Dough.jpg"
+  },{
+    "name":"Cookies N Cream",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Cookies-N-Cream.jpg"
+  },
+  {
+    "name":"Cotton Candy",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Cotton-Candy.jpg"
+  },
+  {
+    "name":"Jamoca Almond\nFudge",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Jamoca-Almond-Fudge.jpg"
+  },
+  {
+    "name":"Love Portion",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Love-Portion-31.jpg"
+  },
+  {
+    "name":"Mint Chocolate\nChip",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Mint-Chocolate-Chip.jpg"
+  },
+  {
+    "name":"Peanut Butter\nN Chocolate",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Peanut-Butter-N-Chocolate.jpg"
+  },
+  {
+    "name":"Rainbow Sherbet",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Rainbow-Sherbet.jpg"
+  },
+  {
+    "name":"Strawberry\nCheese Cake",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"Strawberry-Cheese-Cake.jpg"
+  },
+  {
+    "name":"Very Berry\nStrawberry",
+    "brand":"Baskin Robbins",
+    "price":2.79,
+    "image":"verY-berry-strawberry.jpg"
+  }
+];
